fb26e5391ef0f008aae94f4403dbf0c1e596e540
图丫 <grs59680@gmail.com>
2023-06-11 23:03:49 +0800

    添加登录页

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,5 @@
 </template>
 
 <style scoped>
-.btn {
-  @apply bg-purple-500 
-  text-indigo-50 px-4 py-2 transition-all duration-500 rounded-full hover:(bg-purple-900) focus:(ring-8 ring-purple-900);
-}
 
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,18 @@
-import {createRouter, createWebHashHistory} from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Index from '~/pages/index.vue'
+import NotFound from '~/pages/404.vue'
+import Login from '~/pages/login.vue'
 
 const routes = [{
     path: "/",
     component: Index
+}, {
+    path: "/login",
+    component: Login
+}, {
+    path: "/:pathMatch(.*)*",
+    name: 'NotFound',
+    component: NotFound
 }]
 
 const router = createRouter({
