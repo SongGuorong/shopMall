0a5e424c3140d96f02964f22c2f12e7a4cd748ef
图丫 <grs59680@gmail.com>
2023-06-12 23:25:33 +0800

    输入框图标

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,13 +1,14 @@
 <template>
   <!-- flex布局 -->
   <el-row class="min-h-screen bg-indigo-500">
-    <el-col :span="16" class="flex items-center justify-center">
+    <!-- 响应式布局 -->
+    <el-col :lg="16" :md="12" class="flex items-center justify-center">
       <div>
         <div class="text-light-50 font-bold text-5xl mb-4">欢迎光临</div>
         <div class="text-gray-200 text-sm">此站点为《vue3 + vite实战商城后台开发》演示地址</div>
       </div>
     </el-col>
-    <el-col :span="8" class=" bg-light-50 flex justify-center items-center flex-col">
+    <el-col :lg="8" :md="12" class=" bg-light-50 flex justify-center items-center flex-col">
       <h2 class="font-bold text-3xl text-gray-800">欢迎回来</h2>
       <div class="flex items-center justify-center my-5 text-gray-300 space-x-2">
         <span class="h-[1px] w-16 bg-gray-200"></span>
@@ -16,13 +17,13 @@
       </div>
       <el-form :model="form" class="w-[250px]">
         <el-form-item>
-          <el-input v-model="form.username" placeholder="请输入用户名"/>
+          <el-input :prefix-icon="User" v-model="form.username" placeholder="请输入用户名"/>
         </el-form-item>
         <el-form-item>
-          <el-input v-model="form.password" placeholder="请输入密码"/>
+          <el-input :prefix-icon="Lock" v-model="form.password" show-password placeholder="请输入密码"/>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" round class="w-[250px] bg-indigo-500" @click="onSubmit">登 录</el-button>
+          <el-button type="primary" round color="#626aef" class="w-[250px]" @click="onSubmit">登 录</el-button>
         </el-form-item>
       </el-form>
     </el-col>
@@ -32,7 +33,7 @@
 
 <script setup>
 import { reactive } from 'vue'
-
+import {User, Lock} from '@element-plus/icons-vue'
 // do not use same name with ref
 const form = reactive({
   username: '',
