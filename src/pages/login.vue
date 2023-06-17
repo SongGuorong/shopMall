b8c406e01df56867ae7a6c5db3783e8da1a7f7d8
图丫 <grs59680@gmail.com>
2023-06-17 22:26:06 +0800

    样式抽离

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,19 +1,19 @@
 <template>
   <!-- flex布局 -->
-  <el-row class="min-h-screen bg-indigo-500">
+  <el-row class="login-container">
     <!-- 响应式布局 -->
-    <el-col :lg="16" :md="12" class="flex items-center justify-center">
+    <el-col :lg="16" :md="12" class="left">
       <div>
-        <div class="text-light-50 font-bold text-5xl mb-4">欢迎光临</div>
-        <div class="text-gray-200 text-sm">此站点为《vue3 + vite实战商城后台开发》演示地址</div>
+        <div class="title">欢迎光临</div>
+        <div class="subtitle">此站点为《vue3 + vite实战商城后台开发》演示地址</div>
       </div>
     </el-col>
-    <el-col :lg="8" :md="12" class=" bg-light-50 flex justify-center items-center flex-col">
-      <h2 class="font-bold text-3xl text-gray-800">欢迎回来</h2>
-      <div class="flex items-center justify-center my-5 text-gray-300 space-x-2">
-        <span class="h-[1px] w-16 bg-gray-200"></span>
+    <el-col :lg="8" :md="12" class="right">
+      <h2 class="title">欢迎回来</h2>
+      <div class="subtitle">
+        <span class="line"></span>
         <span>账号密码登录</span>
-        <span class="h-[1px] w-16 bg-gray-200"></span>
+        <span class="line"></span>
       </div>
       <el-form :model="form" class="w-[250px]">
         <el-form-item>
@@ -30,7 +30,6 @@
   </el-row>
 </template>
 
-
 <script setup>
 import { reactive } from 'vue'
 import {User, Lock} from '@element-plus/icons-vue'
@@ -45,3 +44,37 @@ const onSubmit = () => {
 }
 
 </script>
+
+<style scoped>
+.login-container {
+  @apply min-h-screen bg-indigo-500;
+}
+
+.login-container .left, .login-container .right {
+  @apply flex justify-center items-center;
+}
+
+.left .title {
+  @apply text-light-50 font-bold text-5xl mb-4;
+}
+
+.left .subtitle {
+  @apply text-gray-200 text-sm;
+}
+
+.right {
+  @apply bg-light-50 flex-col;
+}
+
+.right .title {
+  @apply font-bold text-3xl text-gray-800;
+}
+
+.right .subtitle {
+  @apply flex items-center justify-center my-5 text-gray-300 space-x-2;
+}
+
+.line {
+  @apply h-[1px] w-16 bg-gray-200;
+}
+</style>
