c2bcaf56af0ac7111c18e9adab29c65589eb672b
SongGuorong <guorong.song@bl.com>
2023-06-18 10:51:09 +0800

    语法糖及响应式处理

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,4 +2,27 @@
     <div>
         后台首页
     </div>
+    <el-button @click="add"> {{ count }} </el-button>
+    <el-button type="primary" @click="increase"> {{ form.count }} </el-button>
+    <NotFound />
 </template>
+
+<script setup>
+import NotFound from '~/pages/404.vue'
+import {reactive, ref} from 'vue'
+
+const count = ref(0)
+
+function add() {
+    count.value++
+}
+
+const form = reactive({
+    count: 0
+})
+
+function increase() {
+    form.count++
+}
+
+</script>
